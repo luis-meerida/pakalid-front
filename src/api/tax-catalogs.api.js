223c21b18685b02ba7db5e59745751f1e7e51687
GrimmFStack <luisalfonsoekpech@gmail.com>
2025-07-19 16:22:57 -0600

    tengo que escribir commits pq mrd copilot ya se acabó

--- src/api/tax-catalogs.api.js
+++ src/api/tax-catalogs.api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:3000/tax-catalogs';
+const API_BASE = 'http://localhost:3002/tax-catalogs';
 
 export const getTaxRegimes = async () => {
   const response = await axios.get(`${API_BASE}/tax-regimes`);
