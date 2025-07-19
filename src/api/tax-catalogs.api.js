5748bfb38c9ee4197256547804d217d0d98774a2
GrimmFStack <luisalfonsoekpech@gmail.com>
2025-07-19 13:44:15 -0600

    pa catalogos

---
+++ src/api/tax-catalogs.api.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const API_BASE = 'http://localhost:3000/tax-catalogs';
+
+export const getTaxRegimes = async () => {
+  const response = await axios.get(`${API_BASE}/tax-regimes`);
+  return response.data;
+};
+
+export const getCfdiUses = async () => {
+  const response = await axios.get(`${API_BASE}/cfdi-uses`);
+  return response.data;
+};
